f81fc8d79f0b3b82291db02f10d326a15676fc21
sumanmoktan <sumantamang662@gmail.com>
2026-05-22 18:44:08 +0545

    adding connect cloudinary in server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,36 @@
-const app = require('./app');
-const DatabaseConnection = require('./db/Database');
+const app = require("./app");
+const DatabaseConnection = require("./db/Database");
+const connectCloudinary = require("./config/cloudinary");
+S;
 
 //Handling uncaught Exception
-process.on("uncaughtException", (err) =>{
-    console.log(`Error: ${err.message}`);
-    console.log(`shutting down server for handling uncaught exception`);
-})
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`shutting down server for handling uncaught exception`);
+});
 
 //config
-if(process.env.NODE_ENV !== "PRODUCTION"){
-    require('dotenv').config({
-        path: "backend/config/.env"
-    });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
 }
 
-//connection 
+//connection
 DatabaseConnection();
+connectCloudinary();
 
 //create server
-const server = app.listen(process.env.PORT, ()=>{
-    console.log(`server is running on http://localhost:${process.env.PORT}`);
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server is running on http://localhost:${process.env.PORT}`);
 });
 
 // handling promise rejection
-process.on("unhandledRejection", (err)=>{
-    console.log(`Error:${err.message}`);
-    console.log(`shutting down a server for unhandling rejection`);
+process.on("unhandledRejection", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log(`shutting down a server for unhandling rejection`);
 
-    server.close(()=>{
-        process.exit(1);
-    })
-})
+  server.close(() => {
+    process.exit(1);
+  });
+});
